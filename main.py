e66410b867cde77dc0e3fa3edf20192c0e1a3192
KyeongSoo Ko <star77sa@naver.com>
2024-06-08 14:34:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ blog_url="https://star77sa.github.io/"
 feed = feedparser.parse(blog_url+"/index.xml")
  
 markdown_text = """
-![header](https://capsule-render.vercel.app/api?type=waving&color=0000FF&height=250&section=header&text=Kyeongsoo%20Ko&fontColor=FFFFFF&fontSize=70&fontAlign=50)
+![header](https://capsule-render.vercel.app/api?type=waving&color=0000FF&height=250&section=header&text=KyeongSoo%20Ko&fontColor=FFFFFF&fontSize=70&fontAlign=50)
 
 
 - Name : KyeongSoo Ko         
